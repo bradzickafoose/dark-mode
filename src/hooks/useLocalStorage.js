252f152411fc683431588d6a20cac3ba5e8a7109
Brad <brad@viziworx.com>
2019-10-30 17:30:02 -0400

    Built a function called useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+	const [ storedValue, setStoredValue ] = useState(() => {
+		if (window.localStorage.getItem(key)) {
+			return JSON.parse(window.localStorage.getItem(key));
+		}
+		window.localStorage.setItem(key, JSON.stringify(initialValue));
+		return initialValue;
+	});
+};
