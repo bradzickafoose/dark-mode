f3c5c63facc746e0bd3c5546a7791719feec16b3
Brad <brad@viziworx.com>
2019-10-30 17:36:42 -0400

    Created a setter function and returned in array

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -8,4 +8,11 @@ const useLocalStorage = (key, initialValue) => {
 		window.localStorage.setItem(key, JSON.stringify(initialValue));
 		return initialValue;
 	});
+
+	const setValue = (value) => {
+		setStoredValue(value);
+		window.localStorage.setItem(key, JSON.stringify(value));
+	};
+
+	return [ storedValue, setValue ];
 };
