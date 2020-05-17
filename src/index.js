17e3647dc8c57325522f3e21df3654aab5370f05
Brad <brad@viziworx.com>
2020-05-17 15:05:04 -0400

    adds url constant and replaces console.log with console.error

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ const App = () => {
   const [coinData, setCoinData] = useState([]);
 
   useEffect(() => {
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+
     axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .get(url)
+      .then(response => setCoinData(response.data))
+      .catch(error => console.error(error.message));
   }, []);
+
   return (
     <div className="App">
       <Navbar />
