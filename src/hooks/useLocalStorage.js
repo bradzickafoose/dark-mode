225d00aca2aaeaaddc291c6b258a6cc61821cfd9
Brad <brad@viziworx.com>
2020-05-17 14:51:53 -0400

    fixes tab spacing

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
-	const [storedValue, setStoredValue] = useState(() => {
+  const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
-	});
+  });
 
-	const setValue = value => {
-		setStoredValue(value);
-		window.localStorage.setItem(key, JSON.stringify(value));
-	};
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
 
-	return [storedValue, setValue];
+  return [storedValue, setValue];
 };
 
 export default useLocalStorage;
